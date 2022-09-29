b2ca5c6011111e9a06d592e5d9bd71dc439b624d
mrchernova <cherraniram@gmail.com>
2022-09-29 17:35:34 +0300

    servlet class

---
+++ src/main/java/by/trucking/TruckingServlet.java
@@ -0,0 +1,47 @@
+package by.trucking;
+
+
+import by.trucking.model.Order;
+import by.trucking.repository.OrderRepositoryDBImpl;
+import by.trucking.service.OrderService;
+import by.trucking.service.OrderServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TruckingServlet extends HttpServlet {
+    private final OrderService os = new OrderServiceImpl(new OrderRepositoryDBImpl());
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println(req);
+        super.service(req, res);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+           // List<Order> orders = os.getById(1);
+        try {
+            os.getById(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        PrintWriter writer = response.getWriter();
+        //writer.write(orders.toString());
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
