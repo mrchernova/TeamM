2ec6859cae33827af08828cefa9276786cf15266
cherraniram@gmail.com <cherraniram@gmail.com>
2022-10-31 21:28:00 +0300

    hibernate -

--- src/main/java/by/trucking/dao/EngineDAO.java
+++ src/main/java/by/trucking/dao/EngineDAO.java
@@ -1,7 +1,8 @@
 package by.trucking.dao;
 
-import com.sun.istack.NotNull;
+//import com.sun.istack.NotNull;
 //import com.sun.istack.internal.NotNull;
+import com.sun.istack.NotNull;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import by.trucking.model.Engine;
@@ -9,19 +10,14 @@ import by.trucking.model.Engine;
 
 public class EngineDAO implements DAO<Engine, String> {
 
-    /**
-     * Connection factory to database.
-     */
+
     private final SessionFactory factory;
 
     public EngineDAO(@NotNull final SessionFactory factory) {
         this.factory = factory;
     }
 
-    /**
-     * Create new engine in engines table.
-     * @param engine for add.
-     */
+
     @Override
     public void create(@NotNull final Engine engine) {
         try (final Session session = factory.openSession()) {
@@ -32,11 +28,7 @@ public class EngineDAO implements DAO<Engine, String> {
         }
     }
 
-    /**
-     * Get engine by model.
-     * @param model for select.
-     * @return engine with param model.
-     */
+
     @Override
     public Engine read(@NotNull final String model) {
         try (final Session session = factory.openSession()) {
@@ -46,10 +38,7 @@ public class EngineDAO implements DAO<Engine, String> {
         }
     }
 
-    /**
-     * Update engine state.
-     * @param engine new state.
-     */
+
     @Override
     public void update(@NotNull final Engine engine) {
         try (Session session = factory.openSession()) {
@@ -60,19 +49,13 @@ public class EngineDAO implements DAO<Engine, String> {
         }
     }
 
-    /**
-     * Delete engine.
-     *
-     * @param engine for delete.
-     */
+
     @Override
     public void delete(@NotNull final Engine engine) {
         try (Session session = factory.openSession()) {
 
             session.beginTransaction();
-
             session.delete(engine);
-
             session.getTransaction().commit();
         }
     }
